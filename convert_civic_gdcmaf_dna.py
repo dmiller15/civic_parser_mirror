84ed0457c8d5b6c8388dc21948590bfaed53b80c
Nam Sy Vo <namsyvo@uchicago.edu>
2019-04-15 12:16:19 -0500

    feat(mapping): clean code for dna mapping

--- convert_civic_gdcmaf_dna.py
+++ convert_civic_gdcmaf_dna.py
@@ -2,13 +2,11 @@ import os
 import sys
 
 
-NP_TYPE = {1: 'SNP', 2: 'DNP', 3: 'TNP'}
-
 """
-Extracts the start, stop, variant type, and inframe values from the provided data.
-Mutates alleles based on normalizing the ref and alt alleles.
+Convert variants in VCF format to GDCMAF format.
+Modify from @Kyle Hernandez's code in https://github.com/NCI-GDC/aliquot-maf-tools
 """
-def vcf2maf_loc(position, ref_allele, var_allele):
+def vcf2maf_loc_allele(position, ref_allele, var_allele):
     ref_length, var_length = len(ref_allele), len(var_allele)
 
     # Remove any prefixed reference bps from all alleles, using "-" for simple indels
@@ -20,12 +18,11 @@ def vcf2maf_loc(position, ref_allele, var_allele):
         position += 1
 
     # position variables
-    start, stop, var_type = None, None, None
+    start, stop = None, None
 
     # Handle SNPs, DNPs, TNPs, or anything larger
     if ref_length == var_length:
         start, stop = position, position + var_length - 1
-        var_type = NP_TYPE.get(var_length, 'ONP')
 
     # Handle all indels, including those complex ones which contain substitutions
     elif ref_length != var_length:
@@ -33,18 +30,15 @@ def vcf2maf_loc(position, ref_allele, var_allele):
         if ref_length < var_length:
             start = position - 1 if ref_allele == "-" else position
             stop  = position if ref_allele == "-" else position + ref_length - 1
-            var_type = "INS"
         # Deletions
         else:
             start, stop = position, position + ref_length - 1
-            var_type = "DEL"
 
     maf_var = {
       'start': start,
       'stop': stop,
       'ref_allele': ref_allele,
       'var_allele': var_allele,
-      'var_type': var_type
     }
     return maf_var
 
@@ -60,7 +54,7 @@ for line in fin:
     var = tmp[0].split(":")
     chrom, pos, ref, alt = var[0], int(var[1]), var[2], var[3]
     civic_var_id, civic_gene_id = tmp[3], tmp[4]
-    maf_var = vcf2maf_loc(pos, ref, alt)
+    maf_var = vcf2maf_loc_allele(pos, ref, alt)
     fout.write("\t".join([civic_var_id, civic_gene_id, "gDNA", chrom, str(maf_var["start"]), maf_var["ref_allele"], maf_var["var_allele"]]) + "\n")
     gDNA_varid[civic_var_id] = True
 fin.close()
@@ -77,7 +71,7 @@ for k, v in sorted(cDNA_var.iteritems(), key=lambda x: int(x[1][0])):
     var = k.split(":")
     chrom, pos, ref, alt = var[0], int(var[1]), var[2], var[3]
     civic_var_id, civic_gene_id = cDNA_var[k][0], cDNA_var[k][1]
-    maf_var = vcf2maf_loc(pos, ref, alt)
+    maf_var = vcf2maf_loc_allele(pos, ref, alt)
     fout.write("\t".join([civic_var_id, civic_gene_id, "cDNA", chrom, str(maf_var["start"]), maf_var["ref_allele"], maf_var["var_allele"]]) + "\n")
 
 fout.close()
